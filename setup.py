9e4c66b44d83c4b9f48e598a7636b0d408d8b210
James Stoup <jrstoup@gmail.com>
2019-05-06 15:30:39 -0400

    Bumping the version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="heatwave",
-    version="1.0.1",
+    version="1.0.2",
     description=("A way of visualizing a heat map of a git repo"),
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
